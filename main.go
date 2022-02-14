31b58c0464e7482570e183de72598b06c933d6fe
GNSParvath <93996039+GNSParvath@users.noreply.github.com>
2022-02-14 12:24:46 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fiber-mongo-api/configs"
+	"fiber-mongo-api/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(app)
+
+	routes.AdminontrolRoute(app)
+
+	app.Listen(":6000")
+}
